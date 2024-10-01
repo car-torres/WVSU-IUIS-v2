da6b13bbb2057674e1a472159210aa431f4e5cd0
Kyle <kylehallares73@gmail.com>
2024-10-02 01:10:20 +0800

    Updated

---
+++ lib/features/enrollment_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          // Put the list of widgets here, separated by commas
+          // i.e. TextField(), Column(), Row(), Container()
+          // Icon
+          // Test
+        ],
+      ),
+    );
+  }
+}

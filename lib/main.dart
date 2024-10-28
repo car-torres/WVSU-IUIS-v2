6127790dafd90fdce65456158892ddf735751edf
m3iyo <meiyotogonon982@gmail.com>
2024-10-28 23:47:06 +0800

    renamed files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wvsu_iuis_v2/features/sign_up_page.dart';
+import 'package:wvsu_iuis_v2/features/login_page.dart';
 
 void main() {
   runApp(const MyApp());
